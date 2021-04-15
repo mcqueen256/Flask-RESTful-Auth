4fd6281087d63816dc44a52a3425afd21dd5db45
Nicholas Buckeridge <bucknich@gmail.com>
2021-04-16 09:30:50 +1000

    Re-

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # flask-restful-api
 
 This is where a long description of our package will go...
+
+# Setup (Mac/Linux)
+
+1. `cd Flask-RESTful-Auth`
+2. `python3 -m venv venv`
+3. `source venv/bin/activate`
+4. `pip install -r requirements.txt`
--- flask_restful_auth/__init__.py
+++ flask_restful_auth/__init__.py
@@ -1,3 +1,8 @@
-__version__ = "0.1.0"
+# This file is used to export classes, modules and functions and store
+# package-global variables.
+#
+# Otherwise this file should remain mostly empty.
+
+__version__ = "0.0.4"
 
 test_message = "Hello Flask!"
