308052e2c20cb0b4398e19b30b85e3b3303a217c
Prateek Kr. Gupta <30932967+prateek45@users.noreply.github.com>
2021-04-22 19:50:24 +0530

    New updates
    I have Implemented Refresh tokens, solved the memory issue.

--- examples/00_basic/server.py
+++ examples/00_basic/server.py
@@ -1,102 +1,172 @@
-# 
-# Server
-# 
-# Example 0: Global and User Text Editor
-# ======================================
-#
-# This server demonstrates the access control of two endpoints:
-#  - /text/global.txt
-#  - /text/user/<username>.txt
-#
-# All registered users can read and write to the /text/global.txt, however
-# only a user can read and write to their own /text/user/<username>.txt file.
-#
-# The user management endpoints are implemented by the flask_restful_auth
-# package. By default, they include:
-# - /user/signup
-# - /user/login
-# - /user/logout
-
-from flask import Flask
+import os
+from flask import Flask, jsonify, request, make_response
 from flask_sqlalchemy import SQLAlchemy
-from flask_restful_auth import RestfulAuth, login_required
-from flask import request
-from flask_login import UserMixin, login_required
+from functools import wraps
+from passlib.hash import sha256_crypt as sc
+import uuid
+import secrets
+import jwt
+import datetime
 
-def create_app():
-    """ Flask application factory. """
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('Secret')
+secret_key_2 = os.environ.get('Secret_2')
+app.config['SQLALCHEMY _DATABASE_URI'] = 'something'
 
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'my little secret'
-    app.config['SECRET'] = 'my little secret'
+db = SQLAlchemy(app)
+blacklist = []
+class User(db.Model):
+    ''' Setting up User'''
+    id = db.Column(db.Integer, primary_key=True)
+    # User vereification
+    email = db.Column(db.String(300), nullable=False, unique=True)
+    username = db.Column(db.String(60), nullable=False, unique=True)
+    password = db.Column(db.String(300), nullable=False)
+    active = db.Column(db.String(50),nullable=False)
+    token = db.Column(db.String(500),nullable = True)
+    refrsh_token = db.Column(db.String(500),nullable = True)
+    # User details
+    name = db.Column(db.String(50), nullable=False)
 
-    # Initialise Flask-SQLAlchemy
-    db = SQLAlchemy(app)
 
-    app.db = db
+def encoding_password(password):
+    return sc.hash(password])
 
-    # Create the user class.
-    class User(db.Model, UserMixin):
-        ''' Setting up User'''
-        id = db.Column(db.String(36), primary_key=True)
-        is_authenticated = db.Column(db.Boolean, nullable=False)
-        is_active = db.Column(db.Boolean, nullable=False)
-        # User vereification
-        username = db.Column(db.String(60), nullable=False, unique=True)
-        password = db.Column(db.String(300), nullable=False)
-        token = db.Column(db.String(500), nullable=True)
+#check if the invalid token has expired or not
+def invalidate_token():
+    for invalid in blacklist:
+        try:
+            jwt.verify(invalid,app.config['SECRET_KEY'])
+        except:
+            blacklist.remove(invalid)
+#validating refresh token
+def validate_ref_token(token):
+    try:
+        jwt.verify(token,secret_key_2)
+        return True
+    except:
+        return False
+#generating refresh token
+def generate_refresh_token(user):
+    tokn= jwt.encode(
+            {
+                "id":user.id,
+                "iat": datetime.datetime.utcnow(),
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=2)
+            },
+            secret_key_2
+        )
+    return tokn
+#generate access token
+def generate_access_token(user):
+     tokn= jwt.encode(
+            {
+                "id":user.id,
+                "iat": datetime.datetime.utcnow(),
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=2)
+            },
+            secret_key_2
+        )
+    return tokn
 
-    # Create tables
-    db.create_all()
+#creating decorator
+#can be used for email auth
+def require_login(func):
+    @wraps(func)
+    def checking(*args,**kwargs):
+        token = request.args.get('token')
+        ref_token = request.args.get('r_token')
+        invalidate_token()
+        if not token or token in blacklist:
+            return jsonify({'message':  'Missing token'}),403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            verified_user = User.query.filter_by(id = data['id']).first()
+            refresh_case = False
+        except:
+            try:
+                exist_data = jwt.decode(ref_token,secret_key_2)
+                verified_user = User.query.filter_by(id = data['id']).first()
+                refresh_case = True
+            except:
+                return jsonify({'message': 'token is invalid'}),403
+        return func(verified_user,refresh_case,*args, **kwargs)
+    return checking
 
-    import uuid
-    from passlib.hash import sha256_crypt
-    new_user = User(
-        id=str(uuid.uuid4()),
-        is_active=True,
-        is_authenticated=False,
-        password=sha256_crypt.hash('password'),
-        username='u1',
-    )
+@app.route('/',methods=['GET','POST'])
+def index():
+    return 'Welcome to chatbox'
+
+#go to user home
+@app.route('/home/<token><r_token>',methods=['GET','POST'])
+@require_login
+def user_home(current_user,ref_case):
+    if current_user.active == "True":
+        if ref_case:
+            current_user.token = generate_access_token(current_user)
+            db.session.commit()
+        return render_template("user_home.html",token=token)
+
+#create/register a user
+@app.route('/register',methods=['GET','POST'])
+def sign_up():
+    data = request.form
+
+    #securing the password
+    passw = encoding_password(data['password'])
+   
+    #create new user
+    new_user = User(id=uuid.uuid4(), name = data['name'],active = "False",password = passw, email = data['email'],username = data['username'])
+   
     #add to database
     db.session.add(new_user)
     db.session.commit()
-
-    # Initialise Flask-RESTful-Auth
-    auth = RestfulAuth(app, User)
-
-    @app.route('/')
-    def index():
-        return "Index page"
-
-    @app.route('/text/global.txt', methods=['GET', 'POST', 'PUT'])
-    # @login_required 
-    def resource_global():
-        print(request.data)
-        if request.method == 'GET':
-            #read the contents of the file and return it
-            with open('global.txt', 'rb') as fin:
-                return fin.read()
-        if request.method in ['POST', 'PUT']:
-            #read the contents of the file and return it
-            with open('global.txt', 'wb') as fout:
-                #arguments for write
-                fout.write(request.data)
-                return "ok"
-        return "TODO"
-
-    @app.route('/text/user/<username>.txt')
-    @auth.login_required
-    def resouce_user(username):
-        return "TODO"
     
-    # Print the url list in the console.
-    if app.config['DEBUG'] is not None:
-        print(app.url_map)
-    
-    return app
+    return '<h1>New User Created</h1>'
 
-# Start development web server
+#login to user section
+@app.route('/login',methods=['GET','POST','PUT'])
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('verfication failed',401)
+
+    user = User.query.filter_by(username = auth.username).first()
+
+    if not user:
+        return make_response('Either username or Password is incorrect. Please try Again!!!',401)
+
+    if sc.verify(auth.password,user.password):
+        user.active = "True"
+        if validate_ref_token(user.refrsh_token):
+            user.token = generate_access_token(user)
+        else:
+            user.token = generate_access_token(user)
+            user.refrsh_token = generate_refresh_token(user)
+        
+        db.session.commit()
+        return redirect(url_for('home', token = user.token, r_token = user.refrsh_token))
+
+#logout of user section
+@app.route('/logout/<token><r_token>',methods=['GET','POST','PUT'])
+@require_login
+def logout(current_user):
+    if current_user.active == "True":
+        try:
+            jwt.verify(token,app.config['SECRET_KEY']) #checking if the token is still valid or not
+            blacklist.append(token)
+        except:
+            print("access token expired") #invalidating the token if not expired yet
+
+    current_user.active = "False"
+    current_user.token = None
+    db.session.commit()
+    
+    return redirect(url_for('/'))
+
+
+        
+    
 if __name__ == '__main__':
-    app = create_app()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
