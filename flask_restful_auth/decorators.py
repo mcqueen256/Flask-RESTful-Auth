8b7a22a46d478f730c37944d5da79bc8215f20eb
Prateek Kumar Gupta <prateek.gupta326@gmail.com>
2021-05-08 17:07:36 +0530

    Resolving conflicts

--- flask_restful_auth/decorators.py
+++ flask_restful_auth/decorators.py
@@ -4,9 +4,26 @@ import jwt
 from functools import wraps
 
 def login_required(func):
-    """
-    Its use for checking the token in args exists in our database or not 
-    """
+     """
+            **Create Teacher Record**
+            This function allows user to create(post) a teacher record.
+            :return: teacher's information added by the user in json and http status code
+            - Example::
+                curl -X POST http://localhost:5000/ -H 'cache-control: no-cache' -H 'content-type: application/json' \
+                -d '{
+                    "name": "Mary Rose",
+                    "subject": "Biology"
+                }'
+            - Expected Success Response::
+                HTTP Status Code: 201
+                {
+                    "name": "Mary Rose",
+                    "subject": "Biology"
+                }
+            - Expected Fail Response::
+                HTTP Status Code: 400
+                {'error': 'Duplicate teacher name'}
+        """
     @wraps(func)
     def checking(*args,**kwargs):
         restful_auth = current_app.restful_auth
